7ed7402d3dd500b2c6409d083bab1a87bef0e216
zeekay <zeekayy@gmail.com>
2011-11-02 16:33:09 -0500

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
+rm ~/.zshrc
 ln -s ~/.zsh/zshrc ~/.zshrc
 echo '# Automatically sourced from ~/.zshrc, add your local customizations here.' > ~/.zsh/local/zshrc
-echo 'Zsh dot files installed, add localized customizations to ~/.zsh/local/zshrc.'
