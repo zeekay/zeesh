da60dea513d896993a1b2e88d48de44af19b56a4
zeekay <zeekayy@gmail.com>
2011-11-03 07:14:14 +0000

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -1,25 +1,31 @@
 #!/bin/sh
 echo "setting up zsh config"
-BASEDIR="$( cd -P "$( dirname "$0" )" && pwd )"
 
-# Remove previous configuration and link to destination if not cloned directly there
-if [ "$BASEDIR" != "$HOME/.zsh" ]; then
+# if setup is run from dot-files/setup.sh use basedir provided
+if [ "$repo_basedir" ]; then
+    basedir=$repo_basedir
+else
+    basedir="$( cd -p "$( dirname "$0" )" && pwd )"
+fi
+
+# remove previous configuration and link to destination if not cloned directly there
+if [ "$basedir" != "$home/.zsh" ]; then
     echo "...removing previous configuration"
     rm -rf ~/.zsh
     rm ~/.zshrc
 
     echo "...linking ~/.zsh and ~/.zshrc"
-    ln -s $BASEDIR ~/.zsh
-    ln -s $BASEDIR ~/.zshrc
+    ln -s $basedir ~/.zsh
+    ln -s $basedir ~/.zshrc
 else
     echo "...removing previous configuration"
     rm ~/.zshrc
 
     echo "...linking ~/.zshrc"
-    ln -s $BASEDIR ~/.zshrc
+    ln -s $basedir ~/.zshrc
 fi
 
 echo "...creating ~/.zsh/local/zshrc for local customizations"
-echo '# Automatically sourced from ~/.zshrc, add your local customizations here.' > ~/.zsh/local/zshrc
+echo '# automatically sourced from ~/.zshrc, add your local customizations here.' > ~/.zsh/local/zshrc
 
 echo "...done!"
