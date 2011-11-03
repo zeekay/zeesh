d96a8febc069969995042de93d8ed71d269d5dca
zeekay <zeekayy@gmail.com>
2011-11-03 05:16:47 +0000

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
-ln -s ~/.zsh/zshrc ~/.zshrc
+
+echo "Setting up zsh config"
+BASEDIR="$( cd -P "$( dirname "$0" )" && pwd )"
+
+echo "...removing previous configuration"
+# don't remove configuration files if cloned directly into destination
+if [ "$BASEDIR" != "$HOME/.zsh" ]; then
+    rm -rf ~/.zsh
+fi
+rm ~/.zshrc
+
+echo "...linking ~/.zsh and ~/.zshrc"
+ln -s $BASEDIR ~/.zsh
+ln -s $BASEDIR/zshrc ~/.zshrc
+
+echo "...creating ~/.zsh/local/zshrc for local customizations"
 echo '# Automatically sourced from ~/.zshrc, add your local customizations here.' > ~/.zsh/local/zshrc
-echo 'Zsh dot files installed, add localized customizations to ~/.zsh/local/zshrc.'
+
+echo "...done!"
