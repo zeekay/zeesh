558e9cadeee9c440920d38cface04bed14696561
zeekay <zeekayy@gmail.com>
2011-11-04 15:12:09 -0500

    Updated login_info

--- plugins/login_info/plugin.zsh
+++ plugins/login_info/plugin.zsh
@@ -1,6 +1,10 @@
 # login_info zeesh plugin
 if [[ -o interactive ]]; then
     versions=(`~/.dot-files/version.sh`)
-    in_array zeesh_plugins themes && theme=":: `cat .zsh/local/theme.last`"
-    echo "\e[1mzsh $ZSH_VERSION :: $versions $theme\e[0m"
+    in_array zeesh_plugins themes && theme=`cat .zsh/local/theme.last`
+    if [ $theme ]; then
+        echo "\e[1mzsh $ZSH_VERSION :: $versions :: $theme\e[0m"
+    else
+        echo "\e[1mzsh $ZSH_VERSION :: $versions\e[0m"
+    fi
 fi
