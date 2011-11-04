7d242ee60fde66a99ee8e5ee0a64c1ad8d14f7ed
zeekay <zeekayy@gmail.com>
2011-11-04 17:37:28 -0500

    Speed up loading with completion

--- plugins/autocomplete/plugin.zsh
+++ plugins/autocomplete/plugin.zsh
@@ -3,10 +3,7 @@
 # add zeesh comps to $fpath
 fpath=( ~/.zsh/comp $fpath )
 
-#autoload -U ~/.zsh/comp/*(:t)
-autoload -U compinit; compinit
-
-zmodload -i zsh/complist
+autoload -U compinit; compinit -C
 
 zstyle ':completion:*' use-cache on
 zstyle ':completion:*' cache-path ~/.zsh/cache
@@ -21,13 +18,7 @@ zstyle ':completion:*:(all-|)files' ignored-patterns '_*' 'zeesh_*'
 zstyle ':completion:*:*:open:*' matcher 'm:{a-z}={A-Z} r: ||[^ ]=**'
 
 # case-insensitive (all), partial-word and then substring completion
-if [ "x$CASE_SENSITIVE" = "xtrue" ]; then
-  zstyle ':completion:*' matcher-list 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-  unset CASE_SENSITIVE
-else
-  zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-fi
-
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 zstyle ':completion:*' list-colors ''
 zstyle ':completion:*:*:*:*:*' menu select
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
