37cbec9d7cecd52134200e9613abdd4201754e64
zeekay <zeekayy@gmail.com>
2011-11-04 15:03:50 -0500

    Added in_array function and updated login_info plugin

--- plugins/login_info/plugin.zsh
+++ plugins/login_info/plugin.zsh
@@ -1,16 +1,6 @@
 # login_info zeesh plugin
 if [[ -o interactive ]]; then
-    DOTFILE_VERSION=$(hg id -n .)
-    # check if using color-switch or oh-my-zsh
-    if [[ -n $COLOR_SWITCH_THEME && -n $OH_MY_ZSH_THEME ]]; then
-        local theme_msg=":: $COLOR_SWITCH_THEME / $OH_MY_ZSH_THEME"
-    elif  [ -n "$COLOR_SWITCH_THEME" ]; then
-        local theme_msg=":: $COLOR_SWITCH_THEME"
-    elif  [ -n "$OH_MY_ZSH_THEME" ]; then
-        local theme_msg=":: $OH_MY_ZSH_THEME"
-    else
-        local theme_msg=""
-    fi
-
-    echo "\e[1mzsh $ZSH_VERSION :: zeesh $DOTFILE_VERSION $theme_msg\e[0m"
+    versions=(`~/.dot-files/version.sh`)
+    in_array zeesh_plugins themes && theme=":: `cat .zsh/local/theme.last`"
+    echo "\e[1mzsh $ZSH_VERSION :: $versions $theme\e[0m"
 fi
