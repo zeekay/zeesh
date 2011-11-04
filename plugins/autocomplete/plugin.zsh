b79c7cba59cb8784bb112262c81ed30010524e89
zeekay <zeekayy@gmail.com>
2011-11-04 17:32:43 -0500

    Speed up autocomplete plugin loading

--- plugins/autocomplete/plugin.zsh
+++ plugins/autocomplete/plugin.zsh
@@ -33,11 +33,25 @@ zstyle ':completion:*:*:*:*:*' menu select
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
 zstyle ':completion:*:*:*:*:processes' command "ps -u `whoami` -o pid,user,comm -w -w"
 
-# Load known hosts file for auto-completion with ssh and scp commands
-if [ -f ~/.ssh/known_hosts ]; then
-  zstyle ':completion:*' hosts $( sed 's/[, ].*$//' $HOME/.ssh/known_hosts )
-  zstyle ':completion:*:*:(ping|telnet|dig|ssh|scp):*:*' hosts `sed 's/^\([^ ,]*\).*$/\1/' ~/.ssh/known_hosts`
-fi
+# # Load known hosts file for auto-completion with ssh and scp commands
+# if [ -f ~/.ssh/known_hosts ]; then
+#   zstyle ':completion:*' hosts $( sed 's/[, ].*$//' $HOME/.ssh/known_hosts )
+#   zstyle ':completion:*:*:(ping|telnet|dig|ssh|scp):*:*' hosts `sed 's/^\([^ ,]*\).*$/\1/' ~/.ssh/known_hosts`
+# fi
+
+# zstyle ':completion:*' hosts $(awk '/^[^#]/ {print $2 $3" "$4" "$5}' /etc/hosts | grep -v ip6- && grep "^#%" /etc/hosts | awk -F% '{print $2}')
+
+# # ignore completion functions (until the _ignored completer)
+# zstyle ':completion:*:functions' ignored-patterns '_*'
+# zstyle ':completion:*:scp:*' tag-order \
+#    files users 'hosts:-host hosts:-domain:domain hosts:-ipaddr"IP\ Address *'
+# zstyle ':completion:*:scp:*' group-order \
+#    files all-files users hosts-domain hosts-host hosts-ipaddr
+# zstyle ':completion:*:ssh:*' tag-order \
+#    users 'hosts:-host hosts:-domain:domain hosts:-ipaddr"IP\ Address *'
+# zstyle ':completion:*:ssh:*' group-order \
+#    hosts-domain hosts-host users hosts-ipaddr
+# zstyle '*' single-ignored show
 
 # comp options verbose info/errors
 #zstyle ':completion:*' verbose yes
