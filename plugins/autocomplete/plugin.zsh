42dbdf0872b8e4e1759b0e933238b36dc9fd5eef
zeekay <zeekayy@gmail.com>
2011-11-05 15:25:39 -0500

    Speed up autocomplete under cygwin.

--- plugins/autocomplete/plugin.zsh
+++ plugins/autocomplete/plugin.zsh
@@ -22,7 +22,9 @@ zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:
 zstyle ':completion:*' list-colors ''
 zstyle ':completion:*:*:*:*:*' menu select
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
-zstyle ':completion:*:*:*:*:processes' command "ps -u `whoami` -o pid,user,comm -w -w"
+
+# this is incredibly slow on cygwin, why am i doing this?
+# zstyle ':completion:*:*:*:*:processes' command "ps -u `whoami` -o pid,user,comm -w -w"
 
 # # Load known hosts file for auto-completion with ssh and scp commands
 # if [ -f ~/.ssh/known_hosts ]; then
