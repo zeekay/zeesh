0b95f117a80ac1216fcf6678843681616d8d138f
zeekay <zeekayy@gmail.com>
2011-12-22 18:01:03 -0600

    Updated more plugins to use newer conventions
    --HG--
    rename : plugins/python/func/zeesh_python_bootstrap => plugins/python/func/python-bootstrap
    rename : plugins/python/func/zeesh_python_cd_site_packages => plugins/python/func/site-packages-cd
    rename : plugins/python/func/zeesh_python_ls_site_packages => plugins/python/func/site-packages-ls
    rename : plugins/virtualbox/func/zeesh_vbox_manage => plugins/virtualbox/func/vbox-manage
    rename : plugins/virtualbox/func/zeesh_vbox_running => plugins/virtualbox/func/vbox-running
    rename : plugins/virtualbox/func/zeesh_vbox_snapshots => plugins/virtualbox/func/vbox-snapshots
    rename : plugins/virtualbox/func/zeesh_vbox_vms => plugins/virtualbox/func/vbox-vms

--- plugins/django/plugin.zsh
+++ plugins/django/plugin.zsh
@@ -1,32 +1,34 @@
-# django zshfu plugin
+### django / zeesh! plugin
 fpath=( ~/.zsh/plugins/django/func $fpath )
 autoload -U  ~/.zsh/plugins/django/func/*(:t)
 
+## exports
+export _GUNICORN_PID=/tmp/gunicorn.pid
+
+## aliases
 alias manage='./manage.py'
 alias dj='./manage.py'
 alias rs='./manage.py runserver'
-alias djproj=_django_startproj
+alias djproj=django-startproj
 alias djapp='./manage.py startapp'
 alias djsync'./manage.py syncdb'
 alias djshell='./manage.py shell'
 alias djload='./manage.py loaddata fixtures/*'
 
-export _GUNICORN_PID=/tmp/gunicorn.pid
-
-# run gunicorn
-gs(){
+## functions
+gs() {
     python manage.py run_gunicorn -c gunicorn.conf.py
 }
-# kill gunircorn
+
 gsk() {
-    if [ -f "$_GUNICORN_PID" ]; then 
+    if [ -f "$_GUNICORN_PID" ]; then
         kill $(cat $_GUNICORN_PID)
         rm $_GUNICORN_PID
     fi
 }
-# reload gunicorn
+
 gsr() {
-    if [ -f "$_GUNICORN_PID" ]; then 
+    if [ -f "$_GUNICORN_PID" ]; then
         kill -HUP $(cat $_GUNICORN_PID)
     fi
 }
--- plugins/python/plugin.zsh
+++ plugins/python/plugin.zsh
@@ -1,43 +1,41 @@
-# python zshfu plugin
+### python / zeesh! plugin
+
 fpath=( ~/.zsh/plugins/python/func $fpath )
 autoload -U ~/.zsh/plugins/python/func/*(:t)
 
-# python
+compctl -K zeesh_python_ls_site_packages zeesh_python_cd_site_packages
+
+## exports
+export PYTHONSTARTUP=~/.pythonrc
+
+## aliases
 alias py=python
 alias ipy=ipython
 alias pyinstall='python setup.py install'
 alias pystrap=zeesh_python_bootstrap
 alias cds=zeesh_python_cd_site_packages
 alias pipr='pip install -r requirements.txt'
-
-# Start a local SMTP server and dump emails sent to it to the console
 alias smtpconsole='python -m smtpd -n -c DebuggingServer localhost:1025'
-
-# Serve the current folder on port 80
-alias serve_this='python -m SimpleHTTPServer'
-
-# pretty JSON
-function pjson() {
-    python -mjson.tool
-}
-function ipy_pdb() {
-    ipython -pdb -c "%run $1"
-}
-compctl -K zeesh_python_ls_site_packages zeesh_python_cd_site_packages
-
-export PYTHONSTARTUP=~/.pythonrc
-
-pye(){ echo 'exec compile(''"'$@[1,-1]'"'', "<cmdline>", "single")' | python - }
+alias serve-this='python -m SimpleHTTPServer'
 alias pye='noglob pye'
 
-#zstyle -e :urlglobber url-other-schema \
-#'[[ $words[1] == scp ]] && reply=("*") || reply=(http https ftp)'
+## functions
+pjson() {
+    python -mjson.tool
+}
 
-noipy(){
+ipy-pdb() {
+    ipython -pdb -c "%run $1"
+}
+
+noipy() {
     export NOIPYTHON=1
     python
     unset NOIPYTHON
 }
-py-upgrade(){
+
+py-upgrade() {
     pip freeze --local | cut -d = -f 1 | xargs echo pip install -U
 }
+
+pye(){ echo 'exec compile(''"'$@[1,-1]'"'', "<cmdline>", "single")' | python - }
