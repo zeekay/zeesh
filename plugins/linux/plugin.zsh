65d5557792b1ecdf655c244a20e14bd806d07f5e
zeekay <zeekayy@gmail.com>
2011-12-23 02:50:40 -0600

    Make lsg and psg accept arguments

--- plugins/linux/plugin.zsh
+++ plugins/linux/plugin.zsh
@@ -1,4 +1,4 @@
-# linux specific conf
+### linux / zeesh! plugin
 
 setopt extended_glob
 export LS_COLORS='di=1;34:ln=35:so=32:pi=33;40:ex=31:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:'
@@ -7,7 +7,6 @@ alias ls='ls --color=always -AF'
 
 # no core dumps
 ulimit -S -c 0 > /dev/null 2>&1
-
 
 function ps(){
     if [ $1 ]; then
