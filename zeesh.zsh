c0d3c347dd348f50f6fa33c9f48e1f2bdeaae3b0
zeekay <zeekayy@gmail.com>
2012-07-01 16:42:57 -0500

    Clean up zeesh.zsh.

--- zeesh.zsh
+++ zeesh.zsh
@@ -1,4 +1,4 @@
-# basic options
+# zsh options
 setopt  extended_glob       \
         auto_name_dirs      \
         ignore_eof          \
@@ -28,34 +28,13 @@ HISTFILE=~/.zsh/local/history
 HISTSIZE=10000
 SAVEHIST=10000
 
-# default exports
+# exports
 export LANG=en_US.UTF-8
 export LC_CTYPE=en_US.UTF-8
-export PATH=~/.dotfiles/scripts:~/.bin:/usr/local/bin:$PATH
-export EDITOR=vim
-export PAGER=vimpager
+export PATH=~/.dotfiles/scripts:$PATH
 export RSYNC_RSH=ssh
 
-# default aliases
-alias v=vim
-alias vi=vim
-alias vm=vim
-function g() {
-    local servername="`vim --serverlist | head -n 1`"
-    if [ "$servername" ]; then
-        gvim --servername "$servername" --remote-silent $@
-    else
-        gvim $@
-    fi
-}
-function m() {
-    local servername="`vim --serverlist | head -n 1`"
-    if [ "$servername" ]; then
-        mvim --servername "$servername" --remote-silent $@
-    else
-        mvim $@
-    fi
-}
+# aliases
 alias l=ls
 alias d='dirs -lv'
 alias cd/='cd /'
@@ -63,42 +42,39 @@ alias .='cd ~-'
 alias r='pushd +1'
 alias pd='pushd'
 alias po='popd'
-alias md='mkdir -p'
-alias rd='rmdir -p'
+alias md=take
 alias cp='nocorrect cp'
 alias mv='nocorrect mv'
 alias rm='nocorrect rm'
 alias rmf='rm -rf'
+alias ack="ack --color-filename='bold blue' --color-lineno='magenta' --color-match='bold red' -i -a"
 alias grep='grep -i --color=auto'
 alias g=grep
-alias gr='grep -r'
 alias frp=find-replace-recursive
-alias frm-find-remove-recursive
-alias upgrade=zeesh-upgrade
-alias plugin=zeesh-plugin
-alias help='zeesh help'
-alias reload='zeesh reload'
-alias tips='zeesh tips'
 alias zip='zip -r -9'
 alias scp='scp -C'
 alias rscp='rsync --partial --progress --rsh=ssh'
 alias ssh_proxy='ssh -C2qTnN -D 9999'
-alias prefs=zeesh-prefs
 alias speedtest='wget --output-document=/dev/null http://speedtest.wdc01.softlayer.com/downloads/test500.zip'
-alias dfu='df-up'
-alias dfp='df-push'
-alias dfs='df-status'
-alias ack="ack --color-filename='bold blue' --color-lineno='magenta' --color-match='bold red' -i -a"
 alias nc='rlwrap nc'
-alias vcs='zeesh-plugin-enable vcs-info'
 alias curl='noglob curl'
 alias diff='git diff --no-index'
 
-function md(){
-    mkdir -p "$@" @@ cd "$@"
-}
+# zeesh aliases
+alias help='zeesh help'
+alias plugin=zeesh-plugin
+alias prefs=zeesh-prefs
+alias reload='zeesh reload'
+alias tips='zeesh tips'
+alias upgrade=zeesh-upgrade
+alias vcs='zeesh-plugin-enable vcs-info'
 
-# keybindings
+# https://github.com/zeekay/dotfiles aliases
+alias dfu='df-up'
+alias dfp='df-push'
+alias dfs='df-status'
+
+## keybindings
 bindkey "^J" self-insert
 bindkey "\e[3~" delete-char
 bindkey '\e[1~' beginning-of-line
@@ -107,15 +83,8 @@ bindkey '\e[H' beginning-of-line
 bindkey '\e[F' end-of-line
 bindkey '\e[7~' beginning-of-line
 bindkey '\e[8~' end-of-line
-# PageUp / PageDown
-# bindkey '\e[5~' beginning-of-history
-# bindkey '\e[6~' end-of-history
-#bindkey '^[[5~' up-line-or-history
-#bindkey '^[[6~' down-line-or-history
 bindkey '^[[5~' up-line-or-history
 bindkey '^[[6~' down-line-or-history
-#bindkey '^[[A' up-line-or-search
-#bindkey '^[[B' down-line-or-search
 bindkey '^[[A' history-search-backward
 bindkey '^[[B' history-search-forward
 bindkey ' ' magic-space
@@ -124,17 +93,6 @@ bindkey '^R' history-incremental-search-backward
 bindkey -s '^L' "|$PAGER"
 bindkey '^Q' quote-line
 bindkey -s '^N' '> /dev/null 2>&1\n'
-
-fancy-ctrl-z () {
-  if [[ $#BUFFER -eq 0 ]]; then
-    echo ''
-    bg
-    zle redisplay
-  else
-    zle push-input
-  fi
-}
-
 zle -N fancy-ctrl-z
 bindkey '^Z' fancy-ctrl-z
 
