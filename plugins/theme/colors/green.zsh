73b711e0e028a9a8fdea43c5a19c1dc905f5fb7a
Zach Kelling <z@zeekay.io>
2017-06-08 23:33:31 -0500

    Update colorschemes

--- plugins/theme/colors/green.zsh
+++ plugins/theme/colors/green.zsh
@@ -1,16 +1,16 @@
 VIRTUAL_ENV_DISABLE_PROMPT=true
 
 if zeesh-plugin-enabled vcs-info; then
-    export VCS_INFO_BRANCH_FMT='%b'
+    export VCS_INFO_BRANCH_FMT='%g'
 
-    export VCS_INFO_GIT_ACTION_FMT='%F{magenta}‹%b%c%u%0.10i %a%m%s›%f'
-    export VCS_INFO_GIT_FMT='%F{magenta}‹%b%c%u%0.10i%m%s›%f'
+    export VCS_INFO_GIT_FMT='%F{green}‹%g%c%u%0.10i%m%s›%f'
+    export VCS_INFO_GIT_ACTION_FMT='%F{green}‹%g%c%u%0.10i %a%m%s›%f'
 
-    export VCS_INFO_HG_ACTION_FMT='%F{magenta}‹%b%c%u %i %a %m%s›%f'
-    export VCS_INFO_HG_FMT='%F{magenta}‹%b%c%u %i %m%s›%f'
+    export VCS_INFO_HG_FMT='%F{green}‹%g%c%u%0.8i %m%s›%f'
+    export VCS_INFO_HG_ACTION_FMT='%F{green}‹%g%c%u%0.10i %a %m%s›%f'
 
     export VCS_INFO_HG_BOOKMARK_FMT=''
-    export VCS_INFO_HG_REV_FMT='%r'
+    export VCS_INFO_HG_REV_FMT='%r '
 
     export VCS_INFO_STAGED_FMT='+'
     export VCS_INFO_UNSTAGED_FMT='!'
@@ -19,15 +19,13 @@ if zeesh-plugin-enabled vcs-info; then
     source ~/.zsh/plugins/vcs-info/style.zsh
 fi
 
-export ZEESH_THEME_PROMPT_CHAR=${ZEESH_THEME_PROMPT_CHAR:-❯}
-export ZEESH_THEME_NORMAL_CHAR=${ZEESH_THEME_NORMAL_CHAR:-'\n⊙'}
-export ZEESH_THEME_VISUAL_CHAR=${ZEESH_THEME_VISUAL_CHAR:-'\n⊡'}
-export ZEESH_THEME_INSERT_CHAR=${ZEESH_THEME_INSERT_CHAR:-'\n$ZEESH_THEME_PROMPT_CHAR'}
+export ZEESH_THEME_NORMAL_CHAR='\n'
+export ZEESH_THEME_VISUAL_CHAR='\n'
+export ZEESH_THEME_INSERT_CHAR='\n'
+export ZEESH_THEME_PROMPT_CHAR=''
 
 _prompt() {
     local mode=
-    local s=
-
     case $KEYMAP in
         vicmd)
             mode=$ZEESH_THEME_NORMAL_CHAR
@@ -40,10 +38,7 @@ _prompt() {
         ;;
     esac
 
-    # only show user if in ssh connection
-    [[ "$SSH_CONNECTION" != '' ]] && s="%F{magenta}%B%n%b%f%F{magenta}@%f%F{magenta}%B%m%f%b "
-
-    s="$s%F{blue}%B${PWD/$HOME/~}%b%f"
+    local s="%F{green}%g%n%g%f%F{green}@%f%F{green}%g%m%f%g %F{blue}%g${PWD/$HOME/~}%g%f"
 
     # display vcs info
     if [ "$vcs_info_msg_0_" ]; then
@@ -52,10 +47,10 @@ _prompt() {
 
     # print virtualenv name if active
     if [ $VIRTUAL_ENV ]; then
-        s="$s %F{magenta}‹${${(s:/:)VIRTUAL_ENV}[-1]}›%f "
+        s="$s %F{green}‹${${(s:/:)VIRTUAL_ENV}[-1]}›%f "
     fi
 
-    echo -e "$s%F{magenta} $mode$ZEESH_THEME_PROMPT_CHAR%f "
+    echo -e "$s%F{green} $mode$ZEESH_THEME_PROMPT_CHAR%f"
 }
 
 _rprompt() {
